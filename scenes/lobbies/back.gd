a50cfe8505063c458a70ba4193bb500b9e00bbb5
Matt Swieboda <mswiebod@gmail.com>
2020-07-03 20:29:29 -0500

    modifies menu to play -> create, join, back

--- scenes/lobbies/back.gd
+++ scenes/lobbies/back.gd
@@ -3,6 +3,5 @@ extends "res://scripts/label_hover.gd"
 
 func _on_back_gui_input(event : InputEvent):
 	if (event.is_pressed()):
-		print_debug("back!")
 		get_tree().change_scene("res://scenes/menu/menu.tscn")
 
