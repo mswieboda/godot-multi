3aaeeecd6dc7debbb2d8fa9de6c59488c0c4640e
Matt Swieboda <mswiebod@gmail.com>
2020-07-05 19:31:24 -0500

    adds leaving lobby

--- scenes/menu/leave.gd
+++ scenes/menu/leave.gd
@@ -3,8 +3,5 @@ extends "res://scripts/label_hover.gd"
 func _on_gui_input(event : InputEvent):
 	if (event.is_pressed()):
 		var lobby = get_parent()
-		
 		lobby.leave_lobby()
-		lobby.hide()
-		lobby.get_parent().get_node("main").show()
 		reset_hover()
--- scenes/menu/lobby.gd
+++ scenes/menu/lobby.gd
@@ -8,8 +8,8 @@ var players = {}
 var self_player_info = { username = '' }
 
 func _ready():
-	get_tree().connect('network_peer_disconnected', self, '_network_peer_disconnected')
 	get_tree().connect('connected_to_server', self, '_connected_to_server')
+	get_tree().connect('network_peer_disconnected', self, '_network_peer_disconnected')
 
 
 func create_server(username):
@@ -20,6 +20,11 @@ func create_server(username):
 	
 	peer.create_server(DEFAULT_PORT, MAX_PLAYERS)
 	get_tree().set_network_peer(peer)
+	
+	var play = get_parent().get_node("play")
+	
+	play.hide()
+	show()
 	
 	player_connected(1, self_player_info)
 
@@ -36,34 +41,53 @@ func join_server(username):
 
 func _connected_to_server():
 	var id = get_tree().get_network_unique_id()
+	var play = get_parent().get_node("play")
 	
-	player_connected(id, self_player_info)	
-	rpc('player_connected', id, self_player_info)	
+	play.hide()
+	show()
+	
+#	player_connected(id, self_player_info)
+	# tell server a new player connected
+	rpc_id(1, 'new_player_connected', id, self_player_info)	
+
+
+remote func new_player_connected(id, player_info):
+	if get_tree().is_network_server():
+		# add all players for the newly connected player
+		for player_id in players:
+			rpc_id(id, "player_connected", player_id, players[player_id])
+			
+		# tell all clients about the new player
+		player_connected(id, player_info)
+		rpc("player_connected", id, player_info)
 
 
 remote func player_connected(id, player_info):
 	players[id] = player_info
 	print_debug("connected to server (" + str(id) + ")")
 
-	var play = get_parent().get_node("play")
-	
-	play.hide()
-	show()
-
-	var label = Label.new()
-	
-	label.set_name(str(id))
-	label.text = player_info.username
-	
-	add_child(label)
+	render_players_list()
 
 
 func leave_lobby():
 	var id = get_tree().get_network_unique_id()
+	
+	print_debug("leave_lobby")
+	
+	get_tree().set_network_peer(null)
+	_network_peer_disconnected(id)
+	
+	players = {}
+	render_players_list()
+	hide()
+	get_parent().get_node("main").show()
+
+func _network_peer_disconnected(id):
+	print_debug("disconnecting from server (" + str(id) + ")")
+	
 	player_disconnected(id)
 	rpc("player_disconnected", id)
-
-
+	
 remote func player_disconnected(id):
 	if (!players.has(id)):
 		return
@@ -71,5 +95,24 @@ remote func player_disconnected(id):
 	var player_info = players[id]
 	players.erase(id)
 	
-	var player = get_node(str(id))
-	remove_child(player)
+	render_players_list()
+
+
+func render_players_list():
+	var players_list = get_node("players list")
+	
+	if (players_list):
+		remove_child(players_list)
+	
+	players_list = VBoxContainer.new()
+	players_list.set_name("players list")
+
+	for player_id in players:
+		var label = Label.new()
+		
+		label.set_name(str(player_id))
+		label.text = players[player_id].username
+		
+		players_list.add_child(label)
+	
+	add_child(players_list)
