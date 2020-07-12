24286dd679a55204ec206664dccfbfa8200599b2
Matt Swieboda <mswiebod@gmail.com>
2020-07-12 15:13:46 -0500

    improves networked player movement (still choppy)

--- objs/player/player.gd
+++ objs/player/player.gd
@@ -3,6 +3,8 @@ extends KinematicBody
 var gravity = -9.8
 var velocity = Vector3()
 
+puppet var puppet_velocity = Vector3()
+
 export var SPEED = 6
 export var ACCELERATION = 3
 export var DEACCELERATION = 5
@@ -26,40 +28,47 @@ func set_color(color : Color):
 
 
 func movement(delta):
-	var camera_xform = $cam_target/camera.get_global_transform()
-	var dir = Vector3()
 	var is_moving = false
+	var horiz_velocity
 	
-	if Input.is_action_pressed("move_forward"):
-		dir += -camera_xform.basis.z
-		is_moving = true
-	if Input.is_action_pressed("move_backward"):
-		dir += camera_xform.basis.z
-		is_moving = true
-	if Input.is_action_pressed("move_left"):
-		dir += -camera_xform.basis.x
-		is_moving = true
-	if Input.is_action_pressed("move_right"):
-		dir += camera_xform.basis.x
-		is_moving = true
-	
-	dir.y = 0
-	dir = dir.normalized()
-	
-	velocity.y += delta * gravity
-	
-	var horiz_velocity = velocity
-	horiz_velocity.y = 0
-	
-	var new_position = dir * SPEED
-	var accel = DEACCELERATION
-	
-	if dir.dot(horiz_velocity) > 0:
-		accel = ACCELERATION
-	
-	horiz_velocity = horiz_velocity.linear_interpolate(new_position, accel * delta)
-	velocity.x = horiz_velocity.x
-	velocity.z = horiz_velocity.z
+	if is_network_master():
+		var dir = Vector3()
+		var camera_xform = $cam_target/camera.get_global_transform()
+		
+		if Input.is_action_pressed("move_forward"):
+			dir += -camera_xform.basis.z
+			is_moving = true
+		if Input.is_action_pressed("move_backward"):
+			dir += camera_xform.basis.z
+			is_moving = true
+		if Input.is_action_pressed("move_left"):
+			dir += -camera_xform.basis.x
+			is_moving = true
+		if Input.is_action_pressed("move_right"):
+			dir += camera_xform.basis.x
+			is_moving = true
+		
+		dir.y = 0
+		dir = dir.normalized()
+		
+		velocity.y += delta * gravity
+		
+		horiz_velocity = velocity
+		horiz_velocity.y = 0
+		
+		var new_position = dir * SPEED
+		var accel = DEACCELERATION
+		
+		if dir.dot(horiz_velocity) > 0:
+			accel = ACCELERATION
+		
+		horiz_velocity = horiz_velocity.linear_interpolate(new_position, accel * delta)
+		velocity.x = horiz_velocity.x
+		velocity.z = horiz_velocity.z
+		
+		rset("puppet_velocity", velocity)
+	else:
+		velocity = puppet_velocity
 	
 	velocity = move_and_slide(velocity, Vector3(0, 1, 0))
 
