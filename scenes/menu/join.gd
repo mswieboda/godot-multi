585aec2601e7a6ce85dd976c8180d634808f17f2
Matt Swieboda <mswiebod@gmail.com>
2020-07-12 22:28:53 -0500

    adds initial lobbies display

--- scenes/menu/join.gd
+++ scenes/menu/join.gd
@@ -2,7 +2,11 @@ extends "res://scripts/label_hover.gd"
 
 onready var popup = get_node("/root/menu/popup")
 
+
 func _on_gui_input(event : InputEvent):
 	if (event.is_pressed()):
-		popup.show_join()
+		var lobbies = get_parent().get_parent().get_node("lobbies")
+		get_parent().hide()
+		lobbies.show()
+		lobbies.load_lobbies()
 		reset_hover()
