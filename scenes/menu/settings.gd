453374e1d53e635727e453f0a8af89df63a60845
Matt Swieboda <mswieboda@users.noreply.github.com>
2020-08-14 22:06:04 -0500

    finishes fullscreen toggle

--- scenes/menu/settings.gd
+++ scenes/menu/settings.gd
@@ -2,5 +2,14 @@ extends "res://scenes/menu/menu.gd"
 
 onready var main = get_parent().get_node("main")
 
+
+func _ready():
+	$fullscreen/toggle.pressed = OS.window_fullscreen
+
+
 func _on_back_gui_input(event : InputEvent):
 	is_pressed_go_to(event, main)
+
+
+func _on_toggle_toggled(button_pressed):
+	OS.window_fullscreen = button_pressed
